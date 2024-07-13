6cb40d7884d378ba811082d2ddf7f6cf31376d5a
Aakash <chaudharyaakash234@gmail.com>
2024-07-13 19:12:08 +0545

    day1

---
+++ day1.js
@@ -0,0 +1,35 @@
+//Day 1: Variables and data types
+
+//Activity: 1
+//Declaring a number using var
+var number = 1
+console.log(number);
+
+//Declaring a string using let
+let string = 'String'
+console.log(string);
+
+//Activity: 2
+//Using const and assigning a boolean value to it
+const task3 = true
+console.log(task3);
+
+//Activity: 3
+//Using typeof
+let num = 2
+console.log(typeof(num));
+
+let string2 = 'String2'
+console.log(typeof(string2));
+
+let isTrue = false
+console.log(typeof(isTrue));
+
+let obj = {
+    language : 'JS',
+    day : 1
+}
+console.log(typeof(obj));
+
+let arr = [1, 2, 3, 4, 5]
+console.log(typeof(arr));
