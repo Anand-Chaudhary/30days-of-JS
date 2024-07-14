4990a84f2a85ffd175c6397415596e8817f2fae0
Aakash <chaudharyaakash234@gmail.com>
2024-07-14 18:05:54 +0545

    Day 2

---
+++ day2.js
@@ -0,0 +1,41 @@
+//Day 2: Operators
+
+//Activity: 1
+//Arithmetic Operators
+let a = 3
+let b = 4
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Activity: 2
+//Assignment Operator
+let result = 1
+result += a
+console.log(result);
+
+result -= b
+console.log(result);
+
+//Activity: 3
+//Comparision Operator
+console.log('Is a > b :', a>b);
+console.log('Is a < b :', a<b);
+console.log('Is a >= b :', a>=b);
+console.log('Is a <= b :', a<=b);
+console.log('Is a == b :', a==b);
+console.log('Is a === b :', a===b);
+
+//Activity: 4
+//Logical Operator
+let c = 1
+let d = 2
+console.log(c > d && c<d);
+console.log(c > d || c<d);
+console.log(c != d);
+
+//Activity: 5
+//Ternary Operator
+c > d ? console.log('1st number is greater than 2nd') :console.log('1st number is less than 2nd');
