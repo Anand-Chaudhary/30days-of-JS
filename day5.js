136e83c912a8991381d79ccbd333ead253dd0d8e
Aakash <chaudharyaakash234@gmail.com>
2024-07-17 08:45:46 +0545

    Day 5

--- day5.js
+++ day5.js
@@ -3,7 +3,38 @@
 
 //Activity: 1
 //Function declaration
-function name(params) {
-    
+function oddOrEven(a) {
+    if ( a % 2 == 0 ) {
+        console.log(a + " is even");
+    }
+    else{
+        console.log(a + " is odd");
+    }
 }
+oddOrEven(2)
 
+function calcSquare(a) {
+    let sq = a * a;    
+    console.log("Square of " + a + " is " + sq);
+}
+calcSquare(4);
+
+//Activity: 2
+//Function Expression
+const findMax = function findMax(a, b) {
+    if (a > b) {
+        console.log(a + " is the maximum");
+    }
+    else{
+        console.log(b + " is maximum");
+    }
+}
+findMax(5, 6)
+
+const concatStr = function concatStr(str1, str2) {
+    return str1 + str2;
+}
+console.log(concatStr("Good", "Morning"));
+
+//Activity: 3
+//Arrow Function
