e171f7de9d86567e6f491d078cd4409bcefa170e
Aakash <chaudharyaakash234@gmail.com>
2024-07-18 07:44:47 +0545

    Day 6

---
+++ day6.js
@@ -0,0 +1,36 @@
+//Day: 6
+//Arrays
+
+//Activity: 1
+//Array creation and access
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0], arr[4]);
+
+//Activity: 2
+//Array Methods
+arr.push(6);
+arr.pop();
+arr.shift();
+arr.unshift(1)
+console.log(arr);
+
+//Activity: 3
+let mapArr = arr.map((val) => {
+    return val * 2;
+});
+console.log(mapArr);
+
+let filterArr = arr.filter((val) => {
+    return val%2 === 0
+})
+console.log(filterArr);
+
+let reducedArr = arr.reduce((res, curr) => {
+    return res + curr;
+})
+console.log(reducedArr);
+
+//Activity: 4
+//Array Iteration
