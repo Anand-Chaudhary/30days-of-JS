40769627f744e7cd04524b8a62cd88570f3d3bcf
Aakash <chaudharyaakash234@gmail.com>
2024-07-20 12:22:42 +0545

    Day 8

---
+++ day8.js
@@ -0,0 +1,12 @@
+//Day: 8
+//ES6+ Features
+
+//Activity: 1
+//Template Literals
+let name = "Aakash";
+let age = 16;
+console.log(`Hi, I am ${name} and I am ${age} years old`);
+console.log(`Hi \nI am ${name} \nI am ${age} years old`);
+
+//Activity: 2
+//Destructuring
