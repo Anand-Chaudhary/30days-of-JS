0ac5ac96a43b173c3408a528e872a38e4c4970db
Aakash <chaudharyaakash234@gmail.com>
2024-07-21 17:42:32 +0545

    Day 9

---
+++ day9/script.js
@@ -0,0 +1,12 @@
+//Day: 9 
+//DOM Manipulation
+
+//Activity: 1
+//Selecting and Manipulating Element
+document.getElementById('id').innerText = "Hi";
+document.querySelector('.color').style.backgroundColor = "red";
+
+//Activity: 2
+//Creating and Appending Elecmet
+document.createElement('div').append('This div is appended');
+document.getElementsByTagName('ul').createElement('li').append('Appended')
