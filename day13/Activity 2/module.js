d13cb60f1d73b9585dbccd3d52ada31243ab3118
Aakash <chaudharyaakash234@gmail.com>
2024-07-25 18:21:41 +0545

    Day 13

---
+++ day13/Activity 2/module.js
@@ -0,0 +1,16 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function diff(a, b) {
+    return a - b;
+}
+
+export function product(a, b) {
+    return a * b;
+}
+
+export default User = {
+    name : "Aakash",
+    schoool : "D.A.V"
+}
---
+++ day13/Activity 2/script.js
@@ -0,0 +1,6 @@
+import User, { diff, add, product } from "./module.js";
+
+console.log(diff(5, 6));
+console.log(add(4, 5));
+console.log(product(2, 3));
+console.log(User);
