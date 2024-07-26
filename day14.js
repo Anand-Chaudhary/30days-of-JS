86789878732396dca229d09b500d4899d5cfda84
Aakash <chaudharyaakash234@gmail.com>
2024-07-26 18:00:21 +0545

    Day 14

--- day14.js
+++ day14.js
@@ -18,4 +18,20 @@ class Person {
 const person = new Person("Aakash", 16);
 console.log(person);
 person.greet();
-person.updateAge(17)
+person.updateAge(17);
+
+//Activity: 2
+//Class Inheritance
+class student extends Person{
+    constructor(name, age,studentId){
+        super(name, age)
+        this.studentId = studentId;
+    }
+    greet(){
+        super.greet();
+        console.log(`Your student id is ${this.studentId}`);
+    }
+}
+const student1 = new student("Anand", 18, 200);
+console.log(student1.studentId);
+student1.greet();
