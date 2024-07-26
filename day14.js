f289108ec6dd9822a68fbb0319491ec1e8aa402b
Aakash <chaudharyaakash234@gmail.com>
2024-07-26 17:44:33 +0545

    Day 14

--- day14.js
+++ day14.js
@@ -3,3 +3,19 @@
 
 //Activity: 1
 //Class Defination
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log(`Hi, ${this.name} of age: ${this.age}`);
+    }
+    updateAge(updatedAge){
+        console.log(`Updated age is: ${updatedAge}`);
+    }
+}
+const person = new Person("Aakash", 16);
+console.log(person);
+person.greet();
+person.updateAge(17)
