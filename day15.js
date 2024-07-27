be3c84641fcda108b6c1132b168d72b23b477c50
Aakash <chaudharyaakash234@gmail.com>
2024-07-27 14:25:39 +0545

    Day 15

--- day15.js
+++ day15.js
@@ -3,3 +3,32 @@
 
 //Activity: 1
 //Understanding Closures
+function outer() {
+    let fruit = "Mango"
+    function inner() {
+        console.log(`My favourite fruit is ${fruit}`);
+    }
+    inner();
+}
+outer();
+
+function counter() {
+    let counter = 0;
+    function privateCounter(){
+        function increment(){
+            counter++;
+            console.log(counter);
+        }
+        function getValue(){
+            console.log(counter);
+        }
+        increment();
+        increment();
+        getValue();
+    };
+    privateCounter();
+};
+counter();
+
+//Activity: 2
+//Practical Closures
