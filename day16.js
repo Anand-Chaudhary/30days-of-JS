aedf6ca4292413292cc69fd7b844ff8a3802e270
Aakash <chaudharyaakash234@gmail.com>
2024-07-28 12:33:41 +0545

    Day 16

--- day16.js
+++ day16.js
@@ -94,3 +94,43 @@ console.log(countOccurrences([], 1)); // Output: 0
 
 //Activity: 5
 //Tree Traversal
+class TreeNode {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+function inOrderTraversal(node) {
+    if (node === null) {
+        return;
+    }
+    
+    inOrderTraversal(node.left);
+    console.log(node.value);
+    inOrderTraversal(node.right);
+}
+
+const root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+inOrderTraversal(root);
+
+function calculateDepth(node) {
+    if (node === null) {
+        return 0;
+    }
+    
+    const leftDepth = calculateDepth(node.left);
+    const rightDepth = calculateDepth(node.right);
+    
+    return Math.max(leftDepth, rightDepth) + 1;
+}
+
+const roots = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+console.log(calculateDepth(roots)); // Output: 3
+
+const singleNode = new TreeNode(1);
+console.log(calculateDepth(singleNode)); // Output: 1
+
+const emptyTree = null;
+console.log(calculateDepth(emptyTree)); // Output: 0
