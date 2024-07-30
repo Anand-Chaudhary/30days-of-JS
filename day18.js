f4f2afaa2dda06c3e1a1b00f864410af2357ff28
Aakash <chaudharyaakash234@gmail.com>
2024-07-30 07:34:01 +0545

    Day 18

--- day18.js
+++ day18.js
@@ -1,2 +1,66 @@
 //Day: 18
 //Algorithms
+
+//Activity: 1
+//Sorting
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    console.log("Bubble Sort: ", arr);
+}
+
+bubbleSort([64, 34, 25, 12, 22, 11, 90]);
+
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        let temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    console.log("Selection Sort: ", arr);
+}
+
+selectionSort([64, 25, 12, 22, 11]);
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let pivot = arr[Math.floor(arr.length / 2)];
+    let left = [];
+    let right = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== Math.floor(arr.length / 2)) {
+            if (arr[i] < pivot) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
+        }
+    }
+
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+let quickSortedArray = quickSort([10, 7, 8, 9, 1, 5]);
+console.log("Quicksort: ", quickSortedArray);
+
+//Activity: 2
+//Searching
