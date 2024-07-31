09eebdb739a91a41173d51cf9e0ea3f06515fbe7
Aakash <chaudharyaakash234@gmail.com>
2024-07-31 17:15:31 +0545

    Day 19

---
+++ day19.js
@@ -0,0 +1,17 @@
+//Day: 19
+//Regular Expression
+
+//Activity: 1
+//Basic Expression
+const text1 = "I love Javascript. Javascript is a versatile language.";
+const regex1 = /Javascript/g;
+const matches1 = text.match(regex1);
+console.log("Matches for 'Javascript':", matches1);
+
+const text2 = "My phone number is 123-456-7890.";
+const regex2 = /\d+/g;
+const matches2 = text.match(regex2);
+console.log("Matches for digits:", matches2);
+
+//Activity: 2
+//
