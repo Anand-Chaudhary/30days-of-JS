df1eab253e70e18556c0d302d715ea78792c61e2
Aakash <chaudharyaakash234@gmail.com>
2024-07-31 17:17:31 +0545

    Day 19

--- day19.js
+++ day19.js
@@ -14,4 +14,14 @@ const matches2 = text.match(regex2);
 console.log("Matches for digits:", matches2);
 
 //Activity: 2
-//
+//Character Classes and Quantifiers
+const text3 = "Alice went to the Wonderland. Bob was not there.";
+const regex3 = /\b[A-Z][a-z]*\b/g;
+const matches3 = text.match(regex3);
+console.log("Matches for words starting with a capital letter:", matches3);
+
+const text4 = "There are 12 apples, 30 oranges, and 7 bananas.";
+const regex4 = /\d+/g;
+const matches4 = text.match(regex4);
+console.log("Matches for sequences of digits:", matches4);
+
