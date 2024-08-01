0d72ae336259a6a14f40730819f193ba79a4301d
Aakash <chaudharyaakash234@gmail.com>
2024-08-01 20:31:50 +0545

    Day 20

---
+++ day20/index.js
@@ -0,0 +1,30 @@
+// Day 20: LocalStorage and SessionStorage
+// Tasks/Activities:
+
+// Activity 1
+//Understanding LocalStorage
+localStorage.setItem("string", "Test");
+myValue = localStorage.getItem("string");
+console.log(myValue);
+
+let person = {
+  name: "Kalpesh",
+  country: "India",
+  age: 43,
+};
+
+localStorage.setItem("objectPerson", JSON.stringify(person));
+console.log(JSON.parse(localStorage.getItem("objectPerson")));
+
+// Activity 3
+//Understanding SessionStorage
+sessionStorage.setItem("string", "Kalpesh");
+console.log(sessionStorage.getItem("string"));
+
+let task6Object = {
+  language: "JS",
+  version: "1.0",
+  projectName: "JS 30Days Challenge",
+};
+sessionStorage.setItem("object", JSON.stringify(task6Object));
+console.log(JSON.parse(sessionStorage.getItem("object")));
