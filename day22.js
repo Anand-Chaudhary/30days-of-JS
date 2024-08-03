5de3de287a003317aade46c3b7224125142d6ed0
Aakash <chaudharyaakash234@gmail.com>
2024-08-03 18:22:48 +0545

    Day 22

--- day22.js
+++ day22.js
@@ -1,8 +1,5 @@
 
 //Activity 1: Add Two Numbers
-/*Task 1: Solve the “Add Two Numbers” problem on LeetCode.
-Write a function that takes two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each node contains a single digit. Add the two numbers and return the sum as a linked list.
-Create a few test cases with linked lists and log the sum as a linked list.*/
 
 class ListNode {
     constructor(val = 0, next = null) {
@@ -68,11 +65,6 @@ class ListNode {
   l2 = createLinkedList([9, 9, 9, 9]);
   sum = addTwoNumbers(l1, l2);
   printLinkedList(sum);
-
-//Activity 2: Longest Substring Without Repeating Characters
-/*Task 2: Solve the “Longest Substring Without Repeating Characters” problem on LeetCode.
-Write a function that takes a string and returns the length of the longest substring without repeating characters.
-Log the length for a few test cases, including edge cases.*/
 
 function lengthOfLongestSubstring(s) {
     let n = s.length;
