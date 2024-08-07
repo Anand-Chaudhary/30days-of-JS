fd9d39f5a481d3e6b2d48ce949611ba185b8af94
Aakash <chaudharyaakash234@gmail.com>
2024-08-07 22:28:20 +0545

    Day 26

---
+++ day26/index.js
@@ -0,0 +1,23 @@
+// const http = require("http");
+// const express = require("express");
+// const path = require("path");
+// const { Server } = require("socket.io");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// // Socket.io
+// io.on("connection", (socket) => {
+//   socket.on("user-message", (message) => {
+//     io.emit("message", message);
+//   });
+// });
+
+// app.use(express.static(path.resolve("./public")));
+
+// app.get("/", (req, res) => {
+//   return res.sendFile("/public/index.html");
+// });
+
+// server.listen(9000, () => console.log(`Server Started at PORT:9000`));
