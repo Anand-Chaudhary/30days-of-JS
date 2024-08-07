25e8665872d2c5d1e1fa127388aaeefe5f470db1
Aakash <chaudharyaakash234@gmail.com>
2024-08-07 22:28:58 +0545

    Day 26

--- day26/index.js
+++ day26/index.js
@@ -1,23 +1,23 @@
-// const http = require("http");
-// const express = require("express");
-// const path = require("path");
-// const { Server } = require("socket.io");
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// // Socket.io
-// io.on("connection", (socket) => {
-//   socket.on("user-message", (message) => {
-//     io.emit("message", message);
-//   });
-// });
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
 
-// app.use(express.static(path.resolve("./public")));
+app.use(express.static(path.resolve("./public")));
 
-// app.get("/", (req, res) => {
-//   return res.sendFile("/public/index.html");
-// });
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
 
-// server.listen(9000, () => console.log(`Server Started at PORT:9000`));
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
